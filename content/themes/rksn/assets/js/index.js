639bda28a9a298e9852b160ce3f67b854aac9587
Rok Haluzan <haluzan.rok@gmail.com>
2014-11-25 16:36:14 +0100

    add index.js

---
+++ content/themes/rksn/assets/js/index.js
@@ -0,0 +1,242 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* globals jQuery, document */
+(function ($, sr, undefined) {
+    "use strict";
+
+    var $document = $(document),
+
+        // debouncing function from John Hann
+        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+        debounce = function (func, threshold, execAsap) {
+            var timeout;
+
+            return function debounced () {
+                var obj = this, args = arguments;
+                function delayed () {
+                    if (!execAsap) {
+                        func.apply(obj, args);
+                    }
+                    timeout = null;
+                }
+
+                if (timeout) {
+                    clearTimeout(timeout);
+                } else if (execAsap) {
+                    func.apply(obj, args);
+                }
+
+                timeout = setTimeout(delayed, threshold || 100);
+            };
+        };
+
+    $document.ready(function () {
+
+        var $postContent = $(".post-content");
+        $postContent.fitVids();
+
+        function updateImageWidth() {
+            var $this = $(this),
+                contentWidth = $postContent.outerWidth(), // Width of the content
+                imageWidth = this.naturalWidth; // Original image resolution
+
+            if (imageWidth >= contentWidth) {
+                $this.addClass('full-img');
+            } else {
+                $this.removeClass('full-img');
+            }
+        }
+
+        var $img = $("img").on('load', updateImageWidth);
+        function casperFullImg() {
+            $img.each(updateImageWidth);
+        }
+
+        casperFullImg();
+        $(window).smartresize(casperFullImg);
+
+        $(".scroll-down").arctic_scroll();
+
+    });
+
+    // smartresize
+    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+    // Arctic Scroll by Paul Adam Davis
+    // https://github.com/PaulAdamDavis/Arctic-Scroll
+    $.fn.arctic_scroll = function (options) {
+
+        var defaults = {
+            elem: $(this),
+            speed: 500
+        },
+
+        allOptions = $.extend(defaults, options);
+
+        allOptions.elem.click(function (event) {
+            event.preventDefault();
+            var $this = $(this),
+                $htmlBody = $('html, body'),
+                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+                toMove;
+
+            if (offset) {
+                toMove = parseInt(offset);
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+            } else if (position) {
+                toMove = parseInt(position);
+                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+            } else {
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+            }
+        });
+
+    };
+})(jQuery, 'smartresize');
+
+//
+(function() {
+
+    // detect if IE : from http://stackoverflow.com/a/16657946
+    var ie = (function(){
+        var undef,rv = -1; // Return value assumes failure.
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf('MSIE ');
+        var trident = ua.indexOf('Trident/');
+
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        } else if (trident > 0) {
+            // IE 11 (or newer) => return version number
+            var rvNum = ua.indexOf('rv:');
+            rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
+        }
+
+        return ((rv > -1) ? rv : undef);
+    }());
+
+
+    // disable/enable scroll (mousewheel and keys) from http://stackoverflow.com/a/4770179
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    var keys = [32, 37, 38, 39, 40], wheelIter = 0;
+
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+        e.preventDefault();
+        e.returnValue = false;
+    }
+
+    function keydown(e) {
+        for (var i = keys.length; i--;) {
+            if (e.keyCode === keys[i]) {
+                preventDefault(e);
+                return;
+            }
+        }
+    }
+
+    function touchmove(e) {
+        preventDefault(e);
+    }
+
+    function wheel(e) {
+        // for IE
+        //if( ie ) {
+            //preventDefault(e);
+        //}
+    }
+
+    function disable_scroll() {
+        window.onmousewheel = document.onmousewheel = wheel;
+        document.onkeydown = keydown;
+        document.body.ontouchmove = touchmove;
+    }
+
+    function enable_scroll() {
+        window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;
+    }
+
+    var docElem = window.document.documentElement,
+        scrollVal,
+        isRevealed,
+        noscroll,
+        isAnimating,
+        container = document.getElementById( 'container' );
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    function scrollPage() {
+        scrollVal = scrollY();
+
+        if( noscroll && !ie ) {
+            if( scrollVal < 0 ) return false;
+            // keep it that way
+            window.scrollTo( 0, 0 );
+        }
+
+        if( classie.has( container, 'notrans' ) ) {
+            classie.remove( container, 'notrans' );
+            return false;
+        }
+
+        if( isAnimating ) {
+            return false;
+        }
+
+        if( scrollVal <= 0 && isRevealed ) {
+            toggle(0);
+        }
+        else if( scrollVal > 0 && !isRevealed ){
+            toggle(1);
+        }
+    }
+
+    function toggle( reveal ) {
+        isAnimating = true;
+
+        if( reveal ) {
+            classie.add( container, 'modify' );
+        }
+        else {
+            noscroll = true;
+            disable_scroll();
+            classie.remove( container, 'modify' );
+        }
+
+        // simulating the end of the transition:
+        setTimeout( function() {
+            isRevealed = !isRevealed;
+            isAnimating = false;
+            if( reveal ) {
+                noscroll = false;
+                enable_scroll();
+            }
+        }, 1200 );
+    }
+
+    // refreshing the page...
+    var pageScroll = scrollY();
+    noscroll = pageScroll === 0;
+
+    disable_scroll();
+
+    if( pageScroll ) {
+        isRevealed = true;
+        classie.add( container, 'notrans' );
+        classie.add( container, 'modify' );
+    }
+
+    window.addEventListener( 'scroll', function() {
+        if($('#container').hasClass('intro-effect-push')) {
+            scrollPage();
+        }
+    });
+})();
