07447cb9e4923cd9236eb5e83ab6e6a2b5b221c6
Rok Haluzan <haluzan.rok@gmail.com>
2014-12-29 22:39:26 +1300

    caption support + image auto width fix

--- content/themes/rksn/assets/js/index.js
+++ content/themes/rksn/assets/js/index.js
@@ -244,3 +244,12 @@
     trigger.addEventListener( 'click', function() { toggle( 'reveal' ); } );
 
 })();
+
+$(document).ready(function() {
+    $('article.post.content img').each(function() {
+        if($(this).attr('caption'))
+            $(this).wrap('<figure class="image"></figure>')
+                .after('<figcaption>'+$(this).attr("caption")+'</figcaption>');
+        });
+    });
+});
